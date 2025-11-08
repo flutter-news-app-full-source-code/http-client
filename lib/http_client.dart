a3f9d636d5aed3b667319174d2a67fcae43e21a5
fulleni <fulleni@hotmail.com>
2025-11-08 13:57:48 +0100

    refactor: add more dio exports and update documentation
    - Export additional types from dio package: CancelToken, DioException, Interceptor
    - Update comments to improve code documentation

--- lib/http_client.dart
+++ lib/http_client.dart
@@ -3,7 +3,7 @@
 library;
 
 // Export Dio types
-export 'package:dio/dio.dart' show InterceptorsWrapper, Options;
+export 'package:dio/dio.dart' show CancelToken, DioException, Interceptor, InterceptorsWrapper, Options;
 
 // Export the main client class
 export 'src/http_client.dart';
